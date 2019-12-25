36864bd8b2430835b97a7ccf2a7565905ddc276f
Mingzhang <mingzhangyang2015@gmail.com>
2019-12-24 21:42:25 -0500

    Update createFilterSection.js

--- src/js/ui/createFilterSection.js
+++ src/js/ui/createFilterSection.js
@@ -62,7 +62,7 @@ export default function createFilterSection(datatable) {
       inp.type = 'checkbox';
       let uid = `${datatable._targetId}-filter-value-${filterName}-value-${i}`;
       inp.id = uid;
-      inp.checked = !!obj.selected;
+      //inp.checked = !!obj.selected; // this is redundant
 
       inp.counterpart = obj;
       inp.addEventListener('change', function () {
